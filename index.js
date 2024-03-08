e377b3c6d681523a228d7836f0e1c04adb43b2f4
Haldwing <139230101+Haldwing@users.noreply.github.com>
2024-03-08 14:53:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,71 @@
-const http = require('http');
-const PORT = 3000;
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+import _ from "lodash";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+const app = express();
+const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const blogPosts = [];
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    blog: blogPosts,
+  });
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+app.post("/compose", (req, res) => {
+  const blogPost = {
+    title: req.body["postTitle"],
+    year: req.body["postYear"],
+    content: req.body["postProper"],
+  };
+  blogPosts.push(blogPost);
+  res.redirect("/");
+});
+
+app.get("/compose", (req, res) => {
+  res.render("compose.ejs");
+});
+
+app.get("/blogpost1", (req, res) => {
+  res.sendFile(__dirname + "/public/blog posts/robsheffield.html");
+});
+
+app.get("/blogpost2", (req, res) => {
+  res.sendFile(__dirname + "/public/blog posts/keithharris.html");
+});
+
+app.get("/blogpost3", (req, res) => {
+  res.sendFile(__dirname + "/public/blog posts/rjsmith.html");
+});
+
+app.get("/blogpost4", (req, res) => {
+  res.sendFile(__dirname + "/public/blog posts/karendurbin.html");
+});
+
+app.get("/blogpost5", (req, res) => {
+  res.sendFile(__dirname + "/public/blog posts/lesterbangs1.html");
+});
+
+app.get("/posts/:page", (req, res) => {
+  const reqTitle = _.lowerCase(req.params.page);
+  blogPosts.forEach((post) => {
+    const actTitle = _.lowerCase(post.title);
+    if (reqTitle === actTitle) {
+      res.render("post.ejs", {
+        title: post.title,
+        year: post.year,
+        content: post.content,
+      });
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
